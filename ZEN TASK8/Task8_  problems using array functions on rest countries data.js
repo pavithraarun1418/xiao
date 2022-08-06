c63a40c8de9b1514ad1a6ed0bba89008b845813c
pavithraarun1418 <110596889+pavithraarun1418@users.noreply.github.com>
2022-08-06 14:33:07 +0530

    Add files via upload

---
+++ ZEN TASK8/Task8_  problems using array functions on rest countries data.js
@@ -0,0 +1,14 @@
+Get all the countries from Asia continent /region using Filter function
+
+var request=new XMLHttpRequest();
+request.open('GET','https://restcountries.eu/rest/v2/all','true');
+request.send();
+request.onload=function (){
+    var countryData=JSON.parse(this.response);
+    const asia=countryData.filter((element)=>{
+        if(element.region==='Asia'){
+            return element.name;
+        }
+    })
+    console.log(asia);
+}
